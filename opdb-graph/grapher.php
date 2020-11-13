df465cd6124d746eb3307d8fcae22bcb05c5a15b
leikareipa <drilledicecream@gmail.com>
2020-11-13 06:56:51 +0200

    grapher: Guarantee that the right margin is large enough...
    ...to fir the Y axis labels.

--- opdb-graph/grapher.php
+++ opdb-graph/grapher.php
@@ -2,88 +2,103 @@
 namespace opdb_graph;
 
 /*
- * Tarpeeksi Hyvae Soft 2019 /
- * opdb-graph
+ * 2019, 2020 Tarpeeksi Hyvae Soft
+ * 
+ * Software: opdb-graph
  * 
  * Graphs data from an opdb database file.
  *
  */
 
-function create_graph(int $width, int $height, string $title, string $filename, int $numDays)
+function create_graph(int $graphWidth,
+                      int $graphHeight,
+                      string $graphTitle,
+                      string $dataSrcFilename,
+                      int $numDays)
 {
-    if (!file_exists($filename)) throw new \Exception("Can't find the given database file.");
-    if ($width < 1 || $height < 1) throw new \Exception("Invalid graph dimensions.");
+    if (!file_exists($dataSrcFilename)) throw new \Exception("Can't find the given database file.");
+    if ($graphWidth < 1 || $graphHeight < 1) throw new \Exception("Invalid graph dimensions.");
 
     // Returns the width of the given string in pixels. Note: makes a hardcoded assumption
     // about which font the string uses.
     $string_pixel_width = function(string $string) { return (strlen($string) * imagefontwidth(2)); };
 
-    $graph = imagecreatetruecolor($width, $height);
+    $graph = imagecreatetruecolor($graphWidth, $graphHeight);
     imagefill($graph, 0, 0, imagecolorallocate($graph, 190, 190, 190));
 
     // Get the data to be graphed.
-    $data = fetch_data($filename, $numDays);
+    $data = fetch_data($dataSrcFilename, $numDays);
     if (count($data) <= 0) return $graph;
 
     $colors = ["black"    => imagecolorallocate($graph, 20, 20, 20),
                "gray"     => imagecolorallocate($graph, 165, 165, 165),
                "dimgray"  => imagecolorallocate($graph, 120, 120, 120)];
-    
-    $marginLeft = 14;
-    $marginRight = 50;
-    $marginTop = 27;
-    $marginBottom = 35;
 
-    $graphWidth = ($width - $marginLeft - $marginRight);
-    $graphHeight = ($height - $marginTop - $marginBottom);
-    if ($graphWidth <= 0 || $graphHeight <= 0) return $graph;
+    $maxVal = max(array_column($data, "value"));
+    $minVal = min(array_column($data, "value"));
+    
+    $marginLeft = (imagefontwidth(2) * 2);
+    $marginRight = 14;
+    $marginTop = 29;
+    $marginBottom = 39;
+
+    // Spacing between labels on the X and Y axes.
+    $horizontalLabelInterval = 80;
+    $verticalLabelInterval = 0.125;
+    if ($graphHeight <= 190) $verticalLabelInterval = 0.25;
+    if ($graphHeight <= 110) $verticalLabelInterval = 0.5;
+
+    // Size the right margin so that all value labels on the Y axis can fit.
+    $fit_right_margin_to_y_labels = function($yFraction) use(&$fit_right_margin_to_y_labels, &$marginRight, $graphHeight,
+                                                             $minVal, $maxVal, $verticalLabelInterval)
+    {
+        $value = round($minVal + (($maxVal - $minVal) * $yFraction), 3);
+        $marginRight = max($marginRight, (strlen(strval($value)) + 3) * imagefontwidth(2));
+
+        return ($yFraction >= 1)? 1 : $fit_right_margin_to_y_labels($yFraction + $verticalLabelInterval);
+    }; $fit_right_margin_to_y_labels(0);
+
+    $graphInnerWidth = ($graphWidth - $marginLeft - $marginRight);
+    $graphInnerHeight = ($graphHeight - $marginTop - $marginBottom);
+    if ($graphInnerWidth <= 0 || $graphInnerHeight <= 0) return $graph;
 
     $startTime = $data[count($data)-1]["timestamp"];
     $endTime = $data[0]["timestamp"];
     if ($endTime < $startTime) throw new \Exception("Invalid timestamps detected.");
 
-    $maxVal = max(array_column($data, "value"));
-    $minVal = min(array_column($data, "value"));
-
     // Draw the graph.
     {
         // Print out the title.
-        imagestring($graph, 2, ($width/2 - $string_pixel_width($title)/2), 5, $title, $colors["black"]);
+        imagestring($graph, 2, ($graphWidth/2 - $string_pixel_width($graphTitle)/2), 7, $graphTitle, $colors["black"]);
 
         // Draw horizontal markers at even intervals along the y axis.
         $draw_y_axis_markers = function($yFraction) use(&$draw_y_axis_markers, $marginLeft, $marginRight, $marginBottom,
-                                                        $marginTop, $colors, $graph, $width, $height, $minVal, $maxVal,
-                                                        $graphHeight)
+                                                        $marginTop, $colors, $graph, $graphWidth, $graphHeight, $minVal, $maxVal,
+                                                        $graphInnerHeight, $verticalLabelInterval)
         {
-            $interval = 0.125;
-            if ($height <= 190) $interval = 0.25;
-            if ($height <= 110) $interval = 0.5;
+            $y = (($graphHeight - $marginBottom) - ($graphInnerHeight * $yFraction));
+            $value = round($minVal + (($maxVal - $minVal) * $yFraction), 3);
+            imagestring($graph, 2, ($graphWidth - $marginRight) + 7, ($y - imagefontheight(2)/2), $value, $colors["dimgray"]);
+            imagedashedline($graph, $marginLeft, $y, ($graphWidth - $marginRight), $y, $colors["gray"]);
 
-            $y = (($height - $marginBottom) - ($graphHeight * $yFraction));
-            $value = round($minVal + (($maxVal - $minVal) * $yFraction), 4);
-            imagestring($graph, 2, ($width - $marginRight) + 7, ($y - imagefontheight(2)/2), $value, $colors["dimgray"]);
-            imagedashedline($graph, $marginLeft, $y, ($width - $marginRight), $y, $colors["gray"]);
-
-            return ($yFraction >= 1)? 1 : $draw_y_axis_markers($yFraction + $interval);
+            return ($yFraction >= 1)? 1 : $draw_y_axis_markers($yFraction + $verticalLabelInterval);
         }; $draw_y_axis_markers(0);
 
         // Draw vertical markers at even intervals along the x axis.
-        $draw_x_axis_markers = function($x) use(&$draw_x_axis_markers, $data, $marginLeft, $marginRight,
-                                                $marginBottom, $marginTop, $colors, $graph, $width, $height,
-                                                $string_pixel_width, $graphWidth, $startTime, $endTime)
+        $draw_x_axis_markers = function($x) use(&$draw_x_axis_markers, $data, $marginLeft, $marginRight, $marginBottom,
+                                                $marginTop, $colors, $graph, $graphWidth, $graphHeight, $string_pixel_width,
+                                                $graphInnerWidth, $startTime, $endTime, $horizontalLabelInterval)
         {
             // Guarantee that the graph's left edge gets a vertical marker.
             if ($x < 0) $x = 0;
 
-            $interval = 80;
-
             imagedashedline($graph,
                             ($marginLeft + $x), $marginTop,
-                            ($marginLeft + $x), ($height - $marginBottom) + (($x > 0)? 3 : 0),
+                            ($marginLeft + $x), ($graphHeight - $marginBottom) + (($x > 0)? 3 : 0),
                             $colors["gray"]);
 
             // Find the timestamp that corresponds to the given x coordinate on the graph.
-            $timePerX = ($endTime - $startTime) / $graphWidth;
+            $timePerX = ($endTime - $startTime) / $graphInnerWidth;
             $currentTime = ($startTime + ($x * $timePerX));
 
             // Print out this marker's timestamp.
@@ -94,24 +109,24 @@ function create_graph(int $width, int $height, string $title, string $filename, 
             {
                 imagestring($graph, 2,
                             ($marginLeft + $x - $string_pixel_width($dateString) + imagefontwidth(2)/2),
-                            ($height - $marginBottom) + 5,
+                            ($graphHeight - $marginBottom) + 5,
                             $dateString, $colors["dimgray"]);
 
                 imagestring($graph, 2,
                             ($marginLeft + $x - $string_pixel_width($timeString) + imagefontwidth(2)/2),
-                            ($height - $marginBottom) + 5 + imagefontheight(2),
+                            ($graphHeight - $marginBottom) + 5 + imagefontheight(2),
                             $timeString, $colors["dimgray"]);
             }
             
-            return ($x <= 0)? 1 : $draw_x_axis_markers($x - $interval);
-        }; $draw_x_axis_markers($graphWidth);
+            return ($x <= 0)? 1 : $draw_x_axis_markers($x - $horizontalLabelInterval);
+        }; $draw_x_axis_markers($graphInnerWidth);
 
         // If there's only one data point, draw it on the graph as a point rather than as a line.
         if ($endTime == $startTime)
         {
             imagefilledellipse($graph,
-                               ($width - $marginRight),
-                               ($marginTop + ($graphHeight/2)),
+                               ($graphWidth - $marginRight),
+                               ($marginTop + ($graphInnerHeight/2)),
                                5, 5, $colors["black"]);
         }
         // Otherwise, draw the data as a continuous line that runs through each data point.
@@ -121,22 +136,22 @@ function create_graph(int $width, int $height, string $title, string $filename, 
             // line through the middle of the graph.
             if ($maxVal == $minVal)
             {
-                $verticalMiddle = ($marginTop + ($graphHeight/2));
+                $verticalMiddle = ($marginTop + ($graphInnerHeight/2));
 
                 imageline($graph,
                           $marginLeft, $verticalMiddle,
-                          ($width - $marginRight), $verticalMiddle,
+                          ($graphWidth - $marginRight), $verticalMiddle,
                           $colors["black"]);
             }
             else
             {
-                $xStep = ($graphWidth / ($endTime - $startTime));
-                $yStep = ($graphHeight / ($maxVal - $minVal));
+                $xStep = ($graphInnerWidth / ($endTime - $startTime));
+                $yStep = ($graphInnerHeight / ($maxVal - $minVal));
 
                 for ($i = (count($data) - 1); $i > 0; $i--)
                 {
-                    $y1 = ($graphHeight - (($data[$i-1]["value"] - $minVal) * $yStep));
-                    $y2 = ($graphHeight - (($data[$i]["value"] - $minVal) * $yStep));
+                    $y1 = ($graphInnerHeight - (($data[$i-1]["value"] - $minVal) * $yStep));
+                    $y2 = ($graphInnerHeight - (($data[$i]["value"] - $minVal) * $yStep));
 
                     $x1 = (($data[$i-1]["timestamp"] - $startTime) * $xStep);
                     $x2 = (($data[$i]["timestamp"] - $startTime) * $xStep);
